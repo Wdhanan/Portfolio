2e9dc897ced4d4eca4f3021cecfa0303aa3596cb
Hanan300 <115946741+Hanan300@users.noreply.github.com>
2023-04-22 19:46:44 +0200

    docs file for hosting

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Skill } from '../models/models';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
+
+  skills: Skill[]=[
+    {
+      name:'JAVA/ PYTHON',
+      level:'Expert',
+      rating: 90,
+
+    },
+    {
+      name:'Springboot/ Microprofile',
+      level:'Expert',
+      rating: 90,
+
+    },
+    {
+      name:'Scrum/ Kanba/Agil',
+      level:'Expert',
+      rating: 90,
+
+    },
+    {
+      name:'HTML/CSS/Javascript',
+      level:'Good',
+      rating: 80,
+
+    },
+    {
+      name:'C/C++',
+      level:'Intermediate',
+      rating: 75,
+
+    },
+    {
+      name:'Angular',
+      level:'Good',
+      rating: 80,
+
+    },
+    {
+      name:'MySQL/SQL/ Docker',
+      level:'Good',
+      rating: 80,
+
+    },
+    
+
+
+  ];
+  ngOnInit(): void {
+    
+  }
 
 }
